3f01ea0d2189a7621c503f00b20fb633cc549822
elanurimirgi <116424565+elanurimirgi@users.noreply.github.com>
2026-01-12 22:42:25 +0300

    Refactor RedisStore initialization and retrieval methods
    Refactor RedisStore to streamline initialization and retrieval of students.

--- src/main/java/app/store/RedisStore.java
+++ src/main/java/app/store/RedisStore.java
@@ -1,25 +1,11 @@
-
 package app.store;
-
-import redis.clients.jedis.Jedis;
-import app.model.Student;
-import com.google.gson.Gson;
-
-public class RedisStore {
-    static Jedis jedis;
-    static Gson gson = new Gson();
-
-    public static void init() {
-        jedis = new Jedis("localhost", 6379); // IP ve PORT burada
-        for (int i = 0; i < 10000; i++) {
-            String id = "2025" + String.format("%06d", i);
-            Student s = new Student(id, "Ad Soyad " + i, "Bilgisayar");
-            jedis.set(id, gson.toJson(s));
-        }
-    }
-
-    public static Student get(String id) {
-        String json = jedis.get(id);
-        return gson.fromJson(json, Student.class);
-    }
+import redis.clients.jedis.Jedis; import com.google.gson.Gson;
+import app.model.Student; import java.util.Random;
+public class RedisStore{
+ static Jedis j=new Jedis("localhost",6379); static Gson g=new Gson();
+ public static void init(){for(int i=1;i<=10000;i++){String n="202500"+i;
+ j.set(n,g.toJson(new Student(n,"Student-"+i,dep())));}} 
+ public static String get(String n){return j.get(n);} 
+ static String dep(){String[] d={"Classical Turkish Music","Turkish Folk Music","Computer Engineering"};
+ return d[new Random().nextInt(d.length)];}
 }
